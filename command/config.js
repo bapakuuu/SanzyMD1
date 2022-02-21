d1d3e67e709d05618948c5a55ec6557f32519737
bapakuuu <99638994+bapakuuu@users.noreply.github.com>
2022-02-21 11:01:11 +0700

    Update config.js

--- command/config.js
+++ command/config.js
@@ -15,9 +15,9 @@ global.APIKeys = {
 
 //━━━━━━━━━━━━━━━[ OTHER ]━━━━━━━━━━━━━━━━━//
 
-global.ownername = 'owner - sanzy'
-global.owner = ['6281276698054']
-global.packname = 'Sanzy'
+global.ownername = 'owner - Leenz'
+global.owner = ['6283871822478']
+global.packname = 'Leenz'
 global.author = 'Gemoyy'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
